cda790c53d69620ef50cd75c93df8f2f70c7e64f
koraykip <62715254+koraykip@users.noreply.github.com>
2025-02-27 19:09:44 +0100

    Add HTML structure and FizzBuzz functionality in JavaScript

---
+++ js-fizzbuzz/script.js
@@ -0,0 +1,28 @@
+// When a user inputs a number
+// Loop from 1 to the entered number
+// If the current number is divisible by 3 then print "Fizz"
+// If the current number is divisible by 5 then print "Buzz"
+// If the current number is divisible by 3 and 5 then print "FizzBuzz"
+// Otherwise print the current number
+
+function fizzBuzz() {
+    let answer = parseInt(prompt("Please enter the number you would like to FizzBuzz up to: "));
+
+    if (isNaN(num) || num <= 0) {
+        console.log("Please enter a valid positive number.");
+        return;
+    }
+
+    for (let i = 1; i <= answer; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        } else if (i % 3 === 0) {
+            console.log("Fizz");
+        } else if (i % 5 === 0) {
+            console.log("Buzz");
+        } else {
+            console.log(i);
+        }
+    }
+}
+fizzBuzz();
