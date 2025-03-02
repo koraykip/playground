d38e6767a48b316b1a9783c883deeaa8948b18ee
koraykip <62715254+koraykip@users.noreply.github.com>
2025-03-02 19:46:15 +0100

    Add cat names demonstration to script.js

--- js-calculate-square/script.js
+++ js-calculate-square/script.js
@@ -14,3 +14,20 @@ const clearBtn = document.querySelector("#clear");
 
 calculateBtn.addEventListener("click", calculate);
 clearBtn.addEventListener("click", () => (results.textContent = ""));
+
+// ================================================================0
+
+const cats = ["Pete", "Biggles", "Jasmine"];
+
+let myFavoriteCats = "My cats are called ";
+
+for (let i = 0; i < cats.length; i++) {
+  if (i === cats.length - 1) {
+    // We are at the end of the array
+    myFavoriteCats += `and ${cats[i]}.`;
+  } else {
+    myFavoriteCats += `${cats[i]}, `;
+  }
+}
+
+console.log(myFavoriteCats); // "My cats are called Pete, Biggles, and Jasmine."
